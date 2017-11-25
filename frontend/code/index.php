813b4a19d8db049e92bd33f879741e832083bdf8
EC2 Default User <ec2-user@ip-172-31-87-134.ec2.internal>
2017-11-25 09:38:03 +0000

    add sttm for kurbetenes test

--- frontend/code/index.php
+++ frontend/code/index.php
@@ -3,6 +3,7 @@ echo "<h1> This is the Three Tier Architecture Demo</h1>";
 echo "<h3> Your Web Running Apache with php on Frontend container </h3>";
 echo "<h3> Your APP Running Apache and PHP providing API services for User information on url http://backend/get_user_details </h2>";
 echo "<h3> Your Database running Mysql/Mariadb holding user information in userdb on users Table </h1>";
+echo "<h3> This is test of kurbeternes"
 function httpPost($baseurl)
 {
 $conn = curl_init($baseurl);
